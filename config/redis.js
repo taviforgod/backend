57c7aca6bf2e647d026cbb183ec3da7338a48bf4
taviforgod <taviforgod@outlook.com>
2025-11-10 21:48:33 +0200

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,13 +1,1 @@
 // backend/config/redis.js
-import IORedis from 'ioredis';
-
-let redis;
-if (process.env.REDIS_URL) {
-  redis = new IORedis(process.env.REDIS_URL);
-  redis.on('error', (err) => console.warn('[redis] connection error:', err.message));
-} else {
-  console.warn('[redis] REDIS_URL not set, skipping Redis connection');
-}
-
-export default redis;
-export const getRedisClient = () => redis;
