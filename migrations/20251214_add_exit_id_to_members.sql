7afa5bb7b40a557e3af4f3246cc2c0761cff7b57
taviforgod <taviforgod@outlook.com>
2026-02-06 15:22:26 +0200

    Delete migrations directory

--- migrations/20251214_add_exit_id_to_members.sql
+++
@@ -1,5 +0,0 @@
--- Migration: add exit_id to members to link active inactive exits
-ALTER TABLE members
-  ADD COLUMN IF NOT EXISTS exit_id INTEGER REFERENCES inactive_member_exits(id) ON DELETE SET NULL;
-
-CREATE INDEX IF NOT EXISTS idx_members_exit_id ON members(exit_id);
