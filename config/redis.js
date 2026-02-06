46d7ec9ce89a9e9d2efa0542d6eb7a14ec3d25cb
taviforgod <taviforgod@outlook.com>
2026-02-06 16:52:56 +0200

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,4 +1,12 @@
-import IORedis from 'ioredis';
-const redis = new IORedis(process.env.REDIS_URL || 'redis://localhost:6379');
+import Redis from 'ioredis';
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT, 10),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  tls: {} // Upstash requires TLS
+});
+
 export default redis;
 export const getRedisClient = () => redis;
